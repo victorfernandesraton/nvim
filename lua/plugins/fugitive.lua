98358dceb32ec1219892c53754d4dca368b2bc9f
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-02-07 21:28:18 -0300

    feat: add fugitive commands

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,9 @@
-
 return {
-    "tpope/vim-fugitive", 
+    "tpope/vim-fugitive",
     config = function()
-    
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gpp", ":G push<CR>")
+        vim.keymap.set("n", "<leader>gff", ":G fetch<CR>")
+        vim.keymap.set("n", "<leader>gll", ":G pull<CR>")
     end
 }
