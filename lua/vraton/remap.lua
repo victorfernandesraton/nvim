f50c7a5c1beb0e5bb0619c6c11d98392a0b05b32
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-02-10 13:59:39 -0300

    fix: improve map and colorscheme by defautl

--- lua/vraton/remap.lua
+++ lua/vraton/remap.lua
@@ -2,13 +2,13 @@ vim.g.mapleader = " "
 -- show file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pb", ":Neotree toggle<CR>")
- 
--- copy things from vim to my system
-vim.keymap.set("n", "<leader>y",  "\"+ y")
-vim.keymap.set("v", "<leader>y",  "\"+ y")
-vim.keymap.set("n", "<leader>y",  "\"+ Y")
 
--- select and move highlight lines 
+-- copy things from vim to my system
+vim.keymap.set("n", "<leader>y", "\"+ y")
+vim.keymap.set("v", "<leader>y", "\"+ y")
+vim.keymap.set("n", "<leader>y", "\"+ Y")
+
+-- select and move highlight lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -17,4 +17,3 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
-
