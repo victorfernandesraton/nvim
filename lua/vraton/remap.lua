6a4126d64ce586c8a5a21502a2f5a3d04ea651b7
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-02-16 12:59:45 -0300

    feat: try add dap

--- lua/vraton/remap.lua
+++ lua/vraton/remap.lua
@@ -1,19 +1,47 @@
 vim.g.mapleader = " "
+local keymap = vim.keymap
 -- show file explorer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pb", ":Neotree toggle<CR>")
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("n", "<leader>pb", ":Neotree toggle<CR>")
 
 -- copy things from vim to my system
-vim.keymap.set("n", "<leader>y", "\"+ y")
-vim.keymap.set("v", "<leader>y", "\"+ y")
-vim.keymap.set("n", "<leader>y", "\"+ Y")
+keymap.set("n", "<leader>y", "\"+ y")
+keymap.set("v", "<leader>y", "\"+ y")
+keymap.set("n", "<leader>y", "\"+ Y")
 
 -- select and move highlight lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- copy for external
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+keymap.set("n", "<leader>y", "\"+y")
+keymap.set("v", "<leader>y", "\"+y")
+keymap.set("n", "<leader>d", "\"_d")
+keymap.set("v", "<leader>d", "\"_d")
+
+-- dap
+keymap.set("n", "<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
+keymap.set("n", "<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
+keymap.set("n", "<leader>bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
+keymap.set("n", '<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>")
+keymap.set("n", '<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>')
+keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>")
+keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
+keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
+keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
+keymap.set("n", '<leader>dd', function()
+    require('dap').disconnect(); require('dapui').close();
+end)
+keymap.set("n", '<leader>dt', function()
+    require('dap').terminate(); require('dapui').close();
+end)
+keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
+keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
+keymap.set("n", '<leader>di', function() require "dap.ui.widgets".hover() end)
+keymap.set("n", '<leader>d?',
+    function()
+        local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
+    end)
+keymap.set("n", '<leader>df', '<cmd>Telescope dap frames<cr>')
+keymap.set("n", '<leader>dh', '<cmd>Telescope dap commands<cr>')
+keymap.set("n", '<leader>de', function() require('telescope.builtin').diagnostics({ default_text = ":E:" }) end)
