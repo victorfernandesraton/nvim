5250950b83b320a4264e921d6dac0086bc4a367e
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-03-21 00:19:01 -0300

    feat: improve shotcut for ctrl +p for search file, old habits never should changes after all

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "telescope find files" })
+        vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "telescope find files" })
         vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = "telescope git files" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "telescope grep" })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
