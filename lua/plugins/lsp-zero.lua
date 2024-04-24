3026042ee9dbb46a9b38488d1f0d3b2ef7d7108e
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-04-24 18:52:13 -0300

    feat: enable python autoimport and confg lua as default lsp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -35,13 +35,13 @@ return {
         local lsp_zero = require('lsp-zero')
 
         -- my shotcuts
-        local on_attach = (function(client, bufnr)
+        local on_attach = (function(_, bufnr)
             local opts = { buffer = bufnr, remap = false }
 
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function()vim.diagnostic.open_float() end, opts)
+            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
             vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
@@ -61,11 +61,10 @@ return {
             -- Install these linters, formatters, debuggers automatically
             ensure_installed = {
                 'debugpy',
-                'mypy',
             },
         })
         require('mason-lspconfig').setup({
-            ensure_installed = { 'tsserver', 'rust_analyzer', 'pylsp', 'eslint', 'gopls', 'sqlls', },
+            ensure_installed = { 'tsserver', 'rust_analyzer', 'pylsp', 'eslint', 'gopls', 'sqlls', 'lua_ls' },
             handlers = {
                 lsp_zero.default_setup,
                 lua_ls = function()
@@ -110,6 +109,7 @@ return {
                                     },
                                     -- auto-completion options
                                     jedi_completion = { fuzzy = true },
+                                    rope_autoimport = { enabled = true },
                                     -- import sorting
                                     isort = { enabled = false },
                                 },
