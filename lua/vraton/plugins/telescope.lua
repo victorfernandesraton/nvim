5fb2dadd71729bf18d0e64956e772f7225988041
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-04-30 02:02:14 -0300

    feat:"better telescope"

--- lua/vraton/plugins/telescope.lua
+++ lua/vraton/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    tag = '0.1.6',
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -17,7 +17,7 @@ return {
 
         -- or create your custom action
         local custom_actions = transform_mod({
-            open_trouble_qflist = function(prompt_bufnr)
+            open_trouble_qflist = function(_)
                 trouble.toggle("quickfix")
             end,
         })
@@ -29,7 +29,8 @@ return {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
                         ["<C-j>"] = actions.move_selection_next,     -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+                        ["<C-q>"] = actions.send_to_qflist + custom_actions.open_trouble_qflist,
+                        ["<C-p>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
                         ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
                     },
                 },
@@ -42,7 +43,8 @@ return {
         local keymap = vim.keymap -- for conciseness
 
         keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
         keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
         keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
         local builtin = require('telescope.builtin')
