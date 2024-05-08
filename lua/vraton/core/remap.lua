674e04695dda9b03c9de2c7a6a7251a993c68b78
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-05-08 00:43:45 -0300

    feat: removbe unused lines

--- lua/vraton/core/remap.lua
+++ lua/vraton/core/remap.lua
@@ -11,8 +11,8 @@ keymap.set("v", "<leader>y", "\"+ y")
 keymap.set("n", "<leader>y", "\"+ Y")
 
 -- select and move highlight lines
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 -- copy for external
 keymap.set("n", "<leader>y", "\"+y")
