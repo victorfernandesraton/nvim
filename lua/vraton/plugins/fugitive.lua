2278ee424e039ffe3fbaf11aeab1cb58312500b2
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-05-13 12:54:38 -0300

    feat: better git log

--- lua/vraton/plugins/fugitive.lua
+++ lua/vraton/plugins/fugitive.lua
@@ -5,7 +5,8 @@ return {
         vim.keymap.set("n", "<leader>gpp", ":G push<CR>")
         vim.keymap.set("n", "<leader>gff", ":G fetch<CR>")
         vim.keymap.set("n", "<leader>gll", ":G pull<CR>")
-        vim.keymap.set("n", "<leader>gtt", ":G log --oneline --graph --all --decorate<CR>",
+        vim.keymap.set("n", "<leader>gtt",
+            ":G log --oneline --graph --all --decorate --pretty=format:'%C(blue)%h%C(red)%d | %C(white)%s - %C(cyan)%cn, %C(green)%cr'<CR>",
             { desc = "Fugitive: git tree" })
     end
 }
