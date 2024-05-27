8837c97b4dcb7a195352ee3139bd440bbdd7b2a2
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-05-26 23:56:47 -0300

    feat: ad black and isort for default and update some crap

--- lua/vraton/plugins/lsp/mason.lua
+++ lua/vraton/plugins/lsp/mason.lua
@@ -8,7 +8,12 @@ return {
         local mason = require("mason")
         local mason_tool_installer = require("mason-tool-installer")
         -- enable mason and configure icons
-        mason.setup({})
+        mason.setup({
+            ensure_installed = {
+                "black",
+                "isort"
+            }
+        })
         mason_tool_installer.setup({
             ensure_installed = {
                 "pylint",
