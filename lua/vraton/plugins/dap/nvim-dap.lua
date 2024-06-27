d40f597a5325d78381fa18b364711a1fe8e27cca
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-06-27 01:28:28 -0300

    feat: add vscode and nodejs dap

--- lua/vraton/plugins/dap/nvim-dap.lua
+++ lua/vraton/plugins/dap/nvim-dap.lua
@@ -7,7 +7,7 @@ return {
         -- https://github.com/mfussenegger/nvim-dap
         'mfussenegger/nvim-dap',
 
-        { "williamboman/mason.nvim", opts = { PATH = "append" } },
+        { "williamboman/mason.nvim",   opts = { PATH = "append" } },
         -- https://github.com/theHamsta/nvim-dap-virtual-text
         'theHamsta/nvim-dap-virtual-text',   -- inline variable text while debugging
         -- https://github.com/nvim-telescope/telescope-dap.nvim
@@ -18,6 +18,12 @@ return {
         -- python
         'mfussenegger/nvim-dap-python',
         -- js
+        "mxsdev/nvim-dap-vscode-js",
+        { "microsoft/vscode-js-debug", build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" },
+        {
+            'Joakker/lua-json5',
+            build = './install.sh'
+        }
     },
     opts = {
         controls = {
@@ -137,6 +143,7 @@ return {
             ensure_installed = {
                 -- Update this to ensure that you have the debuggers for the langs you want
                 'python',
+
             },
         }
 
@@ -160,6 +167,47 @@ return {
             pythonPath = get_python_path(),
             console = "integratedTerminal",
         })
+        -- dap for js
+        require("dap-vscode-js").setup({
+            -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+            debugger_path = vim.fn.stdpath('data') .. "/lazy/vscode-js-debug",                                                       -- Path to vscode-js-debug installation.
+            -- debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+            adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
+            -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+            -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+            -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+        })
+        local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+        for _, language in ipairs(js_based_languages) do
+            require("dap").configurations[language] = {
+                {
+                    type = "pwa-node",
+                    request = "launch",
+                    name = "Launch file",
+                    program = "${file}",
+                    cwd = "${workspaceFolder}",
+                },
+                {
+                    type = "pwa-node",
+                    request = "attach",
+                    name = "Attach",
+                    processId = require 'dap.utils'.pick_process,
+                    cwd = "${workspaceFolder}",
+                },
+                {
+                    type = "pwa-chrome",
+                    request = "launch",
+                    name = "Start Chrome with \"localhost\"",
+                    url = "http://localhost:3000",
+                    webRoot = "${workspaceFolder}",
+                    userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
+                }
+            }
+        end
+
+
+
         local keymap = vim.keymap
         -- dap keybinds
         keymap.set("n", "<leader>bb", function() dap.toggle_breakpoint() end, { desc = "DAP toggle breakpoint" })
