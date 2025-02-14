3a83e5865f19828152132992786339f17fa765be
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2025-02-14 08:02:27 -0300

    feat: remove dressing and add info format in lualine

--- lua/vraton/plugins/lsp/conform.lua
+++ lua/vraton/plugins/lsp/conform.lua
@@ -1,9 +1,50 @@
 return {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    keys = {
+        {
+            '<leader>=f',
+            function()
+                -- If autoformat is currently disabled for this buffer,
+                -- then enable it, otherwise disable it
+                if vim.b.disable_autoformat then
+                    vim.cmd 'FormatEnable'
+                    vim.notify 'Enabled autoformat for current buffer'
+                else
+                    vim.cmd 'FormatDisable!'
+                    vim.notify 'Disabled autoformat for current buffer'
+                end
+            end,
+            desc = 'Toggle autoformat for current buffer',
+        },
+        {
+            '<leader>=F',
+            function()
+                -- If autoformat is currently disabled globally,
+                -- then enable it globally, otherwise disable it globally
+                if vim.g.disable_autoformat then
+                    vim.cmd 'FormatEnable'
+                    vim.notify 'Enabled autoformat globally'
+                else
+                    vim.cmd 'FormatDisable'
+                    vim.notify 'Disabled autoformat globally'
+                end
+            end,
+            desc = 'Toggle autoformat globally',
+        },
+    },
     config = function()
         local conform = require("conform")
-
+        toggle_format_on_save = function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            local disable_filetypes = { c = false, cpp = false }
+            return {
+                timeout_ms = 500,
+                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            }
+        end
         conform.setup({
             formatters_by_ft = {
                 go = { "goimports", "gofmt" },
@@ -16,14 +57,8 @@ return {
                 typescriptreact = { "standardjs", "denolsp" },
                 php = { "phpcbf" }
             },
-            format_on_save = {
-                lsp_format = "fallback",
-                timeout_ms = 1000,
-            },
-            format_after_save = {
-                lsp_format = "fallback",
-                async = true,
-            },
+            format_on_save = toggle_format_on_save,
+            format_after_save = toggle_format_on_save,
             formatters = {
                 ruff_organize_imports = {
                     command = 'ruff',
@@ -47,7 +82,27 @@ return {
             }
         })
 
-        vim.keymap.set({ "n", "v" }, "<leader>=", function()
+        vim.api.nvim_create_user_command('FormatDisable', function(args)
+            if args.bang then
+                -- :FormatDisable! disables autoformat for this buffer only
+                vim.b.disable_autoformat = true
+            else
+                -- :FormatDisable disables autoformat globally
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = 'Disable autoformat-on-save',
+            bang = true, -- allows the ! variant
+        })
+
+        vim.api.nvim_create_user_command('FormatEnable', function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = 'Re-enable autoformat-on-save',
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader>==", function()
             conform.format({
                 lsp_fallback = true,
                 async = false,
--- lua/vraton/plugins/lualine.lua
+++ lua/vraton/plugins/lualine.lua
@@ -2,6 +2,30 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup()
+        local function getautofmt()
+            local text = "fmt"
+            if vim.g.disable_autoformat then
+                text = text .. " g:off"
+            else
+                text = text .. " g:on"
+            end
+
+            if vim.b.disable_autoformat then
+                text = text .. " b:off"
+            else
+                text = text .. " b:on"
+            end
+            return text
+        end
+        require('lualine').setup({
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { getautofmt },
+                lualine_z = { "location", "progress" },
+            },
+        })
     end
 }
